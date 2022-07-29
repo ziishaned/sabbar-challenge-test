b34243f88b01b9f2782c29c0ebe84c3500127518
Zeeshan Ahmad <ziishaned@gmail.com>
2022-07-29 21:19:12 +0400

    add filters

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,7 +4,9 @@ import {DocumentContext} from "next/document";
 import {ReactElement, useEffect, useState} from "react";
 import {
     Button,
+    Checkbox,
     Heading,
+    Select,
     Spinner,
     Stack,
     Table,
@@ -26,10 +28,11 @@ import {useRouter} from "next/router";
 type HomeProps = {
     page: number;
     limit: number;
+    isDriver: 'true' | 'false';
 }
 
 function Home(props: HomeProps): ReactElement {
-    const {page, limit} = props;
+    const {page, limit, isDriver} = props;
 
     const toast = useToast();
     const {push: navigate, query} = useRouter();
@@ -53,7 +56,7 @@ function Home(props: HomeProps): ReactElement {
 
     const getCustomers = (): void => {
         setIsLoadingGetCustomersApi(true)
-        axios.get(`/api/customer?limit=${Number(limit)}&page=${Number(page)}`)
+        axios.get(`/api/customer?limit=${Number(limit)}&page=${Number(page)}${isDriver ? `&isDriver=${isDriver}` : ''}`)
             .then((res) => {
                 setCustomers(res.data.customers);
                 setTotalPages(res.data.totalPages);
@@ -88,7 +91,7 @@ function Home(props: HomeProps): ReactElement {
                 onCloseCustomerEditModal={(): void => setToggleCustomerEditModal(!toggleCustomerEditModal)}
             />
 
-            <Stack maxWidth="70%" margin="auto" spacing="16px" mt="16px">
+            <Stack maxWidth="70%" margin="auto" spacing="16px" my="16px">
                 <Stack isInline justifyContent='space-between' alignItems='center'>
                     <Heading as='h3' size='lg'>Customers</Heading>
                     <Button
@@ -106,95 +109,131 @@ function Home(props: HomeProps): ReactElement {
                         <Text>Loading customers...</Text>
                     </Stack>
                 )}
-                <TableContainer>
-                    <Table variant='striped' borderWidth='1px'>
-                        <Thead>
-                            <Tr>
-                                <Th>Name</Th>
-                                <Th>Is Driver?</Th>
-                                <Th>Longitude</Th>
-                                <Th>Latitude</Th>
-                                <Th isNumeric>Number of Rides</Th>
-                                <Th isNumeric>Rating</Th>
-                                <Th/>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {!customers.length && (
-                                <tr>
-                                    <Td colSpan={6} textAlign='center' py="35px">No data to display!</Td>
-                                </tr>
-                            )}
-                            {!!customers.length && (
-                                <>
-                                    {customers.map((customer: Customer): ReactElement => (
-                                        <Tr key={customer._id}>
-                                            <Td>{customer.name || '-'}</Td>
-                                            <Td>{customer.isDriver ? 'Yes' : 'No'}</Td>
-                                            <Td>{customer.locationLongitude || '-'}</Td>
-                                            <Td>{customer.locationLatitude || '-'}</Td>
-                                            <Td isNumeric>{customer.numberOfRides}</Td>
-                                            <Td isNumeric>{customer.rating}</Td>
-                                            <Td>
-                                                <Stack isInline justifyContent='flex-end'>
-                                                    <Button
-                                                        size='sm'
-                                                        boxShadow='sm'
-                                                        onClick={(): void => {
-                                                            setCustomerIdToEdit(customer._id)
-                                                            setToggleCustomerEditModal(!toggleCustomerEditModal);
-                                                        }}
-                                                        colorScheme='blue'
-                                                        leftIcon={<FiEdit/>}
-                                                    >
-                                                        Edit
-                                                    </Button>
-                                                    <Button
-                                                        size='sm'
-                                                        boxShadow='sm'
-                                                        colorScheme='red'
-                                                        leftIcon={<FiTrash/>}
-                                                        onClick={(): void => {
-                                                            setCustomerIdToDelete(customer._id)
-                                                            setToggleCustomerDeleteModal(!toggleCustomerDeleteModal);
-                                                        }}
-                                                    >
-                                                        Delete
-                                                    </Button>
-                                                </Stack>
-                                            </Td>
-                                        </Tr>
-                                    ))}
-                                </>
-                            )}
-                        </Tbody>
-                    </Table>
-                </TableContainer>
-                {!!customers.length && (
-                    <Stack isInline justifyContent='center' spacing='14px'>
-                        <Button
-                            size='sm'
-                            boxShadow='sm'
-                            colorScheme='blue'
-                            isDisabled={page === 1}
-                            onClick={(): void => {
-                                navigate(`/?page=${page - 1}&limit=${limit}`);
-                            }}
-                        >
-                            Previous
-                        </Button>
-                        <Button
-                            size='sm'
-                            boxShadow='sm'
-                            colorScheme='blue'
-                            isDisabled={page === totalPages}
-                            onClick={(): void => {
-                                navigate(`/?page=${page + 1}&limit=${limit}`);
-                            }}
-                        >
-                            Next
-                        </Button>
-                    </Stack>
+                {!isLoadingGetCustomersApi && (
+                    <>
+                        <Stack isInline justifyContent='space-between' alignItems='center'>
+                            <Checkbox
+                                isChecked={isDriver === 'true'}
+                                onChange={(event) => {
+                                    navigate(`/?page=${page}&limit=${limit}&isDriver=${event.target.checked}`);
+                                }}
+                            >
+                                Show drivers only
+                            </Checkbox>
+                            <Stack isInline spacing="16px" alignItems='center' flex={0.4} justifyContent='flex-end'>
+                                <Button
+                                    size='sm'
+                                    boxShadow='sm'
+                                    onClick={() => {
+                                        navigate('/');
+                                    }}
+                                >
+                                    Reset Filters
+                                </Button>
+                                <Select
+                                    width="30%"
+                                    placeholder='Limit'
+                                    onChange={(event): void => {
+                                        navigate(`/?page=${page}&limit=${event.target.value}${isDriver ? `&isDriver=${isDriver}` : ''}`);
+                                    }}
+                                >
+                                    <option value='10'>10</option>
+                                    <option value='20'>20</option>
+                                    <option value='30'>30</option>
+                                </Select>
+                            </Stack>
+                        </Stack>
+                        <TableContainer>
+                            <Table variant='striped' borderWidth='1px'>
+                                <Thead>
+                                    <Tr>
+                                        <Th>Name</Th>
+                                        <Th>Is Driver?</Th>
+                                        <Th>Longitude</Th>
+                                        <Th>Latitude</Th>
+                                        <Th isNumeric>Number of Rides</Th>
+                                        <Th isNumeric>Rating</Th>
+                                        <Th/>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {!customers.length && (
+                                        <tr>
+                                            <Td colSpan={7} textAlign='center' py="35px">No data to display!</Td>
+                                        </tr>
+                                    )}
+                                    {!!customers.length && (
+                                        <>
+                                            {customers.map((customer: Customer): ReactElement => (
+                                                <Tr key={customer._id}>
+                                                    <Td>{customer.name || '-'}</Td>
+                                                    <Td>{customer.isDriver ? 'Yes' : 'No'}</Td>
+                                                    <Td>{customer.locationLongitude || '-'}</Td>
+                                                    <Td>{customer.locationLatitude || '-'}</Td>
+                                                    <Td isNumeric>{customer.numberOfRides}</Td>
+                                                    <Td isNumeric>{customer.rating}</Td>
+                                                    <Td>
+                                                        <Stack isInline justifyContent='flex-end'>
+                                                            <Button
+                                                                size='sm'
+                                                                boxShadow='sm'
+                                                                onClick={(): void => {
+                                                                    setCustomerIdToEdit(customer._id)
+                                                                    setToggleCustomerEditModal(!toggleCustomerEditModal);
+                                                                }}
+                                                                colorScheme='blue'
+                                                                leftIcon={<FiEdit/>}
+                                                            >
+                                                                Edit
+                                                            </Button>
+                                                            <Button
+                                                                size='sm'
+                                                                boxShadow='sm'
+                                                                colorScheme='red'
+                                                                leftIcon={<FiTrash/>}
+                                                                onClick={(): void => {
+                                                                    setCustomerIdToDelete(customer._id)
+                                                                    setToggleCustomerDeleteModal(!toggleCustomerDeleteModal);
+                                                                }}
+                                                            >
+                                                                Delete
+                                                            </Button>
+                                                        </Stack>
+                                                    </Td>
+                                                </Tr>
+                                            ))}
+                                        </>
+                                    )}
+                                </Tbody>
+                            </Table>
+                        </TableContainer>
+                        {!!customers.length && (
+                            <Stack isInline justifyContent='center' spacing='14px'>
+                                <Button
+                                    size='sm'
+                                    boxShadow='sm'
+                                    colorScheme='blue'
+                                    isDisabled={page === 1}
+                                    onClick={(): void => {
+                                        navigate(`/?page=${page - 1}&limit=${limit}${isDriver ? `&isDriver=${isDriver}` : ''}`);
+                                    }}
+                                >
+                                    Previous
+                                </Button>
+                                <Button
+                                    size='sm'
+                                    boxShadow='sm'
+                                    colorScheme='blue'
+                                    isDisabled={page === totalPages}
+                                    onClick={(): void => {
+                                        navigate(`/?page=${page + 1}&limit=${limit}${isDriver ? `&isDriver=${isDriver}` : ''}`);
+                                    }}
+                                >
+                                    Next
+                                </Button>
+                            </Stack>
+                        )}
+                    </>
                 )}
             </Stack>
         </>
@@ -202,9 +241,10 @@ function Home(props: HomeProps): ReactElement {
 }
 
 Home.getInitialProps = (ctx: DocumentContext) => {
-    const {limit = 10, page = 1} = ctx.query;
+    const {limit = 10, page = 1, isDriver} = ctx.query;
 
     return {
+        isDriver,
         page: Number(page),
         limit: Number(limit),
     };
